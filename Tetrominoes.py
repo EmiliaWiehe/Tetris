631472218d8fc9abbf8405723e6de5f8481a8713
EmiliaWiehe <ewiehe80@gmail.com>
2022-06-09 13:27:25 +0200

    first draft

---
+++ Tetrominoes.py
@@ -0,0 +1,36 @@
+# class for each tetrominos
+class Tetrominoes:
+    def __init__():
+        pass
+    
+    # 7 different Tetrominoes
+    # L ___| 
+    def orange_ricky(self):
+        pass
+
+    # J |___
+    def blue_ricky(self):
+        pass
+
+    # I _____ light blue
+    def hero(self):
+        pass
+
+    # T __|__ purple
+    def teewee(self):
+        pass
+
+    # O [] yellow
+    def smashboy(self):
+        pass
+
+    # Z -|_ red
+    def cleveland_z(self):
+        pass
+
+    # S _|- green
+    def rhode_island_z(self):
+        pass
+
+
+
